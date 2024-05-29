fc18d5698ab5e49ddc26a5a61cd0740e955ffe11
lizhang.chen <lizhang.chen@bytedance.com>
2024-05-30 06:57:36 +0800

    add adamw in opts.py

--- toy.py
+++ toy.py
@@ -5,7 +5,7 @@ from torch import nn
 from torchvision.datasets import MNIST, CIFAR10
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from opts import Lion
+from opts import *
 
 class MLP(nn.Module):
   '''
@@ -37,9 +37,10 @@ if __name__ == '__main__':
 
     # Prepare CIFAR-10 dataset
     dataset = CIFAR10(os.getcwd(), download=True, transform=transforms.ToTensor())
-    batch_size = 1000
+    batch_size = 50000
     trainloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1)
     steps_in_epoch = len(dataset) // batch_size
+    print(f"Steps in epoch: {steps_in_epoch}")
     # Initialize the MLP
     mlp = MLP()
 
@@ -47,18 +48,19 @@ if __name__ == '__main__':
     loss_function = nn.CrossEntropyLoss()
 
     # Adam
-    # optimizer = torch.optim.Adam(mlp.parameters(), lr=1e-4)
+    optimizer = AdamW(mlp.parameters(), lr=3e-4, weight_decay=10.)
 
     # Lion
-    optimizer = Lion(mlp.parameters(), lr=1e-4, weight_decay=10.)
+    # optimizer = Lion(mlp.parameters(), lr=1e-4, weight_decay=10.)
 
     # scheduler 
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100)
+    T = 2
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=T)
 
 
     # Run the training loop
     losses = []
-    for epoch in range(0, 100):
+    for epoch in range(0, T):
 
     # Print epoch
         print(f'Starting epoch {epoch+1}')
@@ -90,12 +92,13 @@ if __name__ == '__main__':
             # Print statistics
             current_loss += loss.item()
             if i % steps_in_epoch == steps_in_epoch - 1:
-                print('Loss after mini-batch %5d: %.3f' %
+                print('Loss after mini-batch %5d: %.6f' %
                     (i + 1, current_loss / batch_size))
                 losses.append(current_loss/batch_size)
                 current_loss = 0.0
 
         scheduler.step()
+
     import datetime
     # Print to indicate training completion
     print('Training process has finished.')
